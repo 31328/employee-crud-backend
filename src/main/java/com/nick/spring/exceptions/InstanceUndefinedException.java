1e2b864df31841686836e191fd3d47a95bf3fb1d
unknown <g31328r@gmail.com>
2025-12-11 09:13:18 +0200

    second commit

--- src/main/java/com/nick/spring/exceptions/InstanceUndefinedException.java
+++
@@ -1,8 +0,0 @@
-package com.nick.spring.exceptions;
-
-public class InstanceUndefinedException extends RuntimeException {
-    // generate the following constructor from Super Class RuntimeException
-    public InstanceUndefinedException(String message) {
-        super(message);
-    }
-}
